fd4a65886e5cc4608b098fb0a685bda57317d77d
coutino568 <40869460+coutino568@users.noreply.github.com>
2023-03-28 19:42:18 -0600

    Init

---
+++ LABC.py
@@ -0,0 +1,19 @@
+
+
+filePath1 = "slr-1.yal"
+filePath2 = "slr-2.yal"
+filePath3 = "slr-3.yal"
+filePath4 = "slr-4.yal"
+
+
+
+
+
+
+file = open(filePath1, "r")
+
+print(file.read())
+
+def read():
+	pass
+	
